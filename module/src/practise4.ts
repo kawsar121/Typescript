2c0501f11f673a38eaea3983e55408d8ba8a6704
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2025-12-22 00:10:39 +0600

    arry and object spread oparetors

--- module/src/practise4.ts
+++ module/src/practise4.ts
@@ -1,1 +1,23 @@
-// Spread Oparetorss
+// Spread Oparetors
+
+// Arry Spread
+const peopele : string [] = ['naim', 'abdul', 'jolil', 'rahat']
+const Mens : string [] = ['Najmul', 'Shorif', 'nahid', 'Abid']
+const allPeople = peopele.push(...Mens)
+
+// Object Spread
+const messi ={
+    goal : 52,
+    fullName : 'Adres Messi',
+    sonName : 'sold'
+}
+const ronDa ={
+    goal : 54,
+    fullName : 'Custiar Ronaldo',
+    sonName : 'lop'
+}
+const MessiVsRonda = {
+    ...messi,
+    ...ronDa
+}
+
