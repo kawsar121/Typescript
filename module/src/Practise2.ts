2d812303a92d199434e33254d896981c6bd2fe74
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2025-12-21 20:56:39 +0600

    Object and type literal part

---
+++ module/src/Practise2.ts
@@ -0,0 +1,14 @@
+       // Objects 
+const users : {
+    company : 'bd 71' // Eta ke bole Type--> literal  //eta use korbo tokhon jokhon kono value fixed hobe
+    firstName : string;
+    middleName? : string; //Optional Jodi middle name thake nibe, na thakle o somossa ney
+    lastName : string;
+    age : number;
+} = {
+    firstName : 'Tohidul',
+    middleName : 'Islam',
+    lastName : 'Kawsar',
+    age : 21,
+    company : 'bd 71'
+}
