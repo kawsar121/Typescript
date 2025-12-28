54f732029ce7db2298b62e6916cea9ff89cc5808
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2025-12-28 12:34:33 +0600

    destucturing

---
+++ module/src/practise5.ts
@@ -0,0 +1,11 @@
+    // Destructuring
+const joshim : {
+    name : string,
+    age: number
+} = {
+    name : 'josim uddin',
+    age  : 30
+}
+
+const {name} = joshim;
+const {name : herName} = joshim;   // clone diye name decler kore dilam
